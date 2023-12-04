ad64dd5ff5307bb09e14df308485a7b7661881ac
saikrishnadev95 <saikrishnatsk35@gmail.com>
2023-12-04 15:08:05 +0530

    changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,35 @@
 dnf module disable nodejs -y
+
 dnf module enable nodejs:18 -y
+
 dnf install nodejs -y
+
 useradd roboshop
+
+rm -rf /app
 mkdir /app
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
 cd /app
+
 unzip /tmp/catalogue.zip
+
 npm install
+
 cp  /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+
 systemctl daemon-reload
+
 systemctl enable catalogue
+
 systemctl start catalogue
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 dnf install mongodb-org-shell -y
+
 mongo --host mongodb.saikrishna.cloud	 </app/schema/catalogue.js
+
 
 
