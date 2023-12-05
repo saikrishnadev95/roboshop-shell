85430924d899ac28466449092443460b3a5c4c40
saikrishnadev95 <saikrishnatsk35@gmail.com>
2023-12-05 21:43:35 +0530

    changes

--- mongodb.sh
+++ mongodb.sh
@@ -2,3 +2,5 @@ cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
+sed -i -e 's|127.0.0.1|0.0.0.0.|' /etc/mongod.conf
+systemctl restart mongod
